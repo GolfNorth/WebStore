2623600caf824ec6f1c46954337859b3806f9a6e
GolfNorth <golf155@yandex.ru>
2020-04-20 23:27:00 +0500

    Добавлена валидация модели сотрудников

--- WebStore/Models/EmployeeViewModel.cs
+++ WebStore/Models/EmployeeViewModel.cs
@@ -1,14 +1,31 @@
-﻿using WebStore.Domain.Entities.Base.Interfaces;
+﻿using System.ComponentModel.DataAnnotations;
+using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Models
 {
     public class EmployeeViewModel : IBaseEntity
     {
         public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Имя является обязательным")]
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Фамилия является обязательной")]
+        [StringLength(64, MinimumLength = 2, ErrorMessage = "В фамилии должно быть не менее 2х и не более 64 символов")]
+        [Display(Name = "Фамилия")]
         public string SurName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Отчество является обязательным")]
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Возраст является обязательным")]
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Должность является обязательной")]
+        [Display(Name = "Должность")]
         public string Position { get; set; }
     }
 }
