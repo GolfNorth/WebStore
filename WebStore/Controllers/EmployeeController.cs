634b8073952412ce99130f15c339543b9d354ca3
GolfNorth <golf155@yandex.ru>
2020-04-21 15:54:37 +0500

    Добавлен вывод ошибок валидации сотрудников

--- WebStore/Controllers/EmployeeController.cs
+++ WebStore/Controllers/EmployeeController.cs
@@ -48,6 +48,11 @@ namespace WebStore.Controllers
         // GET: /employees/edit/{id}
         public IActionResult Edit(EmployeeViewModel model)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
             if (model.Id > 0) // если есть Id, то редактируем модель
             {
                 var dbItem = _employeesService.GetById(model.Id);
