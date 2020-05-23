e7556b5d109b1ea25d09ee617f971d00e8050985
GolfNorth <golf155@outlook.com>
2020-05-23 17:35:57 +0500

    Добавление промежуточного ПО, обеспечивающее обработку ошибок и логирование в конвейере обработки входящих запросов

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -15,6 +15,7 @@ using WebStore.Clients.Products;
 using WebStore.Clients.Values;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities.Identity;
+using WebStore.Infrastructure.Middleware;
 using WebStore.Interfaces.Api;
 using WebStore.Interfaces.Services;
 using WebStore.Logger;
@@ -120,6 +121,8 @@ namespace WebStore
 
             app.UseAuthentication();
             app.UseAuthorization();
+            
+            app.UseMiddleware<ErrorHandlingMiddleware>();
 
             app.UseEndpoints(endpoints =>
             {
