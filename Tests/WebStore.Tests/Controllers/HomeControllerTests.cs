4e2326fa28af07686fdd27c719321e0c62ca6c56
GolfNorth <golf155@outlook.com>
2020-05-28 09:50:39 +0500

    Тесты HomeController

---
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Controllers;
+using Xunit;
+
+namespace WebStore.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexReturnsView()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+        
+        [Fact]
+        public void ContactUsReturnsView()
+        {
+            var controller = new HomeController();
+
+            var result = controller.ContactUs();
+
+            Assert.IsType<ViewResult>(result);
+        }  
+    }
+}
