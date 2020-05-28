64765f0e4e2631a6067a42be6a0275a4c3d9941b
GolfNorth <golf155@outlook.com>
2020-05-28 11:39:04 +0500

    Тесты для WebAPITestController

---
+++ Tests/WebStore.Tests/Controllers/WebAPITestControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using WebStore.Controllers;
+using WebStore.Interfaces.Api;
+using Xunit;
+
+namespace WebStore.Tests.Controllers
+{
+    public class WebAPITestControllerTests
+    {
+        [Fact]
+        public void IndexReturnsViewWithValues()
+        {
+            var expectedResult = new[] { "1", "2", "3" };
+            var valueService = Substitute.For<IValueServices>();
+            valueService.Get().Returns(expectedResult);
+            var controller = new WebAPITestController(valueService);
+
+            var result = controller.Index();
+
+            var resultData = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<string>>(resultData.Model);
+            Assert.Equal(expectedResult.Length, model.Count());
+            
+            valueService.Received().Get();
+            //valueService.VerifyNoOtherCalls(); ???
+        }
+    }
+}
