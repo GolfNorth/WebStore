0772505695ac953b7b731b2bc156508b49174ffe
GolfNorth <golf155@outlook.com>
2020-05-31 13:58:53 +0500

    Выделение активной категории

---
+++ Common/WebStore.Domain/ViewModels/CategoryCompleteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WebStore.Domain.ViewModels
+{
+    public class CategoryCompleteViewModel
+    {
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+
+        public int? CurrentCategoryId { get; set; }
+
+        public int? ParentCategoryId { get; set; }
+    }
+}
