8d04a7284a1b48f39a916a81a9ef73f8b6d0b27a
GolfNorth <golf155@outlook.com>
2020-05-31 16:15:47 +0500

    Наполнение хлебных крошек

--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using SmartBreadcrumbs.Nodes;
 using WebStore.Domain.Entities;
 using WebStore.Domain.ViewModels;
 using WebStore.Interfaces.Services;
@@ -11,11 +12,16 @@ namespace WebStore.Controllers
     {
         private readonly IProductService _productService;
         private readonly IMapper _mapper;
+        private MvcBreadcrumbNode _indexNode;
 
         public CatalogController(IProductService productService, IMapper mapper)
         {
             _productService = productService;
             _mapper = mapper;
+            _indexNode = new MvcBreadcrumbNode(
+                "Index",
+                "Catalog",
+                "Каталог");
         }
 
         [Route("shop")]
@@ -30,14 +36,72 @@ namespace WebStore.Controllers
                 CategoryId = categoryId,
                 Products = products.Select(_mapper.Map<ProductViewModel>).OrderBy(p => p.Order)
             };
+            
+            var categoryNode = categoryId != null
+                ? new MvcBreadcrumbNode(
+                    "Index",
+                    "Catalog",
+                    _productService.GetCategory((int) categoryId).Name)
+                {
+                    RouteValues = new
+                    {
+                        categoryId = categoryId
+                    },
+                    Parent = _indexNode
+                }
+                : null;
 
+            var brandNode = brandId != null
+                ? new MvcBreadcrumbNode(
+                    "Index",
+                    "Catalog",
+                    _productService.GetBrand((int) brandId).Name)
+                {
+                    RouteValues = new
+                    {
+                        brandId = brandId
+                    },
+                    Parent = categoryNode ?? _indexNode
+                }
+                : null;
+
+            ViewData["BreadcrumbNode"] = brandNode ?? categoryNode ?? _indexNode;
+            
             return View(model);
         }
-
+        
         [Route("product/{id}")]
         public IActionResult View(int id)
         {
             var product = _productService.GetProduct(id);
+            
+            ViewData["BreadcrumbNode"] = new MvcBreadcrumbNode(
+                "View", 
+                "Catalog", 
+                product.Name)
+            {
+                Parent = new MvcBreadcrumbNode(
+                    "Index",
+                    "Catalog",
+                    product.Brand.Name)
+                {
+                    RouteValues = new
+                    {
+                        brandId = product.Brand.Id
+                    },
+                    Parent = new MvcBreadcrumbNode(
+                        "Index",
+                        "Catalog",
+                        product.Category.Name)
+                    {
+                        RouteValues = new
+                        {
+                            categoryId = product.Category.Id
+                        },
+                        Parent = _indexNode
+                    }
+                }
+            };
 
             return View(_mapper.Map<ProductViewModel>(product));
         }
